62456c3ffdfd7d342e356b73d696f4ef03245947
Craig Livorsi <craig.livorsi@rxrevu.com>
2021-04-07 16:10:07 -0600

    added source.yml and refernced them in models

--- models/orders.sql
+++ models/orders.sql
@@ -8,14 +8,23 @@ payments as (
 
 select * from {{ ref('stg_payments') }}
 
+),
+
+final as (
+
+    select order_id,
+        customer_id,
+        order_date,
+
+        sum(amount_dollars) amount_dollars
+
+    from orders
+
+    left join payments using (order_id)
+
+    where status = 'success'
+    
+    group by 1, 2, 3
 )
 
-
-select order_id,
-    customer_id,
-    order_date,
-    sum(amount_dollars) amount_dollars
-from orders
-    left join payments using (order_id)
-where status = 'success'
-group by 1, 2, 3
+select * from final
--- models/stg_payments.sql
+++ models/stg_payments.sql
@@ -1,4 +1,4 @@
-select id,
+select id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
@@ -6,4 +6,4 @@ select id,
     created,
     _batched_at
 
-from raw.stripe.payment
+from {{ source('stripe', 'payment') }}
